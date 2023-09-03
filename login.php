c639e94277673b2f29381dc7f58f4228b6851f19
Jan Salvador Sebastian <sebastian@mclinica.com>
2023-09-03 11:57:05 +0800

    Added missing piece

--- login.php
+++ login.php
@@ -2,19 +2,21 @@
 
     include 'config.php'; 
 
-    $username = mysqli_real_escape_string($connection, $_POST['username']);
-    $password = mysqli_real_escape_string($connection ,$_POST['password']);
+    $username = "admin"; //mysqli_real_escape_string($connection, $_POST['username']);
+    $password = "admin"; //mysqli_real_escape_string($connection ,$_POST['password']);
 
     
     $sql_query = "SELECT * FROM Users WHERE username='".$username."' AND password='".$password."'";
+    $result = mysqli_query($connection,$sql_query);
+    $row = $result->fetch_assoc();
 
     if (
         $result -> num_rows == 1 && 
         $username == $row['username'] &&
         $password == $row['password'] 
     ) {
-        echo "Successfully Logged In!";
+        echo "success";
     } else {
-        echo "Please try again!";
+        echo "failed";
     }
 ?>
